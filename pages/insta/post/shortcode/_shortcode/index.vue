f4b741f224922574bb4d63801dd7bb5bafbf7ba3
webszy <sunzhenyu@live.com>
2019-07-23 21:44:51 +0800

    通过网址下载的组件

---
+++ pages/insta/post/shortcode/_shortcode/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class='post'>
+    {{username}}
+  </div>
+</template>
+
+
+<script>
+
+export default {
+name:'DownInsPost',
+components:{},
+data(){
+return {
+  username:this.$route.params.shortcode || "NULL"
+  }
+ },
+created(){},
+mounted(){},
+methods:{
+
+ },
+computed:{}
+}
+</script>
+<style>
+
+
+</style>
