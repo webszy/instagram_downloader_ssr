d33b2d2f4808905c87a5412cd568cc5af85a80a8
webszy <sunzhenyu@live.com>
2019-07-23 21:44:25 +0800

    ajax 相关

---
+++ utils/request.js
@@ -0,0 +1,91 @@
+
+import axios from 'axios'
+import {getShareData,getQueryHashByScript,getProfilePageContainerURL} from './tools'
+
+const Service = axios.create({
+    timeout: 7000, // 请求超时时间
+    method:'get',
+
+})
+const InsBaseProfileURL='https://www.instagram.com/',
+InsSearchURL='https://www.instagram.com/web/search/topsearch/?context=blended&query='
+Service.interceptors.response.use(
+        response => {
+            const res = response.data
+          
+            return res
+        },
+        error => {
+            return Promise.reject(error)
+        }
+)
+export function getUserBaseInfo(username){
+  return Service({
+    url:InsBaseProfileURL+username+'/'
+  })
+  .then(res=>{
+    let jsFileURL=getProfilePageContainerURL(res)
+    return getShareData(res,jsFileURL)
+  })
+}
+
+export function getQueryHash(requestUrl){
+    return Service({
+        method: 'get',
+        url: requestUrl,
+    })
+    .then(res=>{
+      return  getQueryHashByScript(res,'queryId')
+    })
+}
+
+export function getNextPageData(userId, end_cursor,queryHash,tagname) {
+     let p={
+        query_hash:queryHash,
+        variables:''
+    },variables={}
+    if(tagname===undefined){
+       variables={
+        id:userId,
+        first:12,
+        after:end_cursor
+      }
+    }else{
+      variables={
+        'tag_name':tagname,
+        first:6,
+        after:end_cursor
+      }
+    }
+   
+    p.variables=JSON.stringify(variables)
+    return Service({
+         url:InsBaseProfileURL+'graphql/query/',
+         params:p
+    })
+}
+export function getSingleMediaInfo(shortCode,username){
+  return Service({
+    url:InsBaseProfileURL+username+'/p/'+shortCode+'/'
+  })
+  .then(res=>{
+    return getShareData(res)
+  })
+}
+export function getHashTagData(tag){
+  return Service({
+    url:InsBaseProfileURL+'explore/tags/'+ tag +'/',
+     headers: {
+        'Content-Type': 'text/html;charset=UTF-8'
+     }
+  })
+  .then(res=>{
+    let jsFileURL=getProfilePageContainerURL(res,'hashtag')
+    return getShareData(res,jsFileURL)
+  })
+}
+export function getInsSearchResult(qs){
+  return Service({
+    url:InsSearchURL+qs
+  })
+}
