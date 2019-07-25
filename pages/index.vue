911d64112dcead083421fea4ef8c13f648c11fba
webszy <sunzhenyu@live.com>
2019-07-25 16:57:19 +0800

    更换searchBar

--- pages/index.vue
+++ pages/index.vue
@@ -3,25 +3,9 @@
     <section class="top">
       <h1>{{platform}}</h1>
       <h2>Media Download</h2>
-      <el-autocomplete  
-        v-model="inputVal" 
-        placeholder="username or post URL" 
-        :trigger-on-focus="false"
-        :fetch-suggestions="querySearchAsync"
-        @select="handleSelect"
-        :popper-append-to-body="false"
-      >
-        <button slot="suffix" class="search" @click="goNextPage">search</button>
-         <template slot-scope="{ item }">
-           <div class="afterQuery">
-             <img :src="item.src" alt="User Avatar">
-             <p>{{ item.name }}</p>
-           </div>         
-        </template>
-      </el-autocomplete >
-      <section class="select"> 
-        <el-radio v-model="platform" label="Instagram">Instagram</el-radio>
-      </section>
+
+      <searchBar></searchBar>
+
       <img class="leftbg" src="../assets/images/instagram_image@2x.png" >
       <img class="rightbg" src="../assets/images/instagram_image@2x.png" >
     </section>
@@ -46,13 +30,15 @@
 
 <script>
 import sFooter from '../components/footer'
+import searchBar from '../components/searchBar'
 import {
   getInsSearchResult
 }from '../utils/request'
 export default {
   name:'HomePage',
   components: {
-    sFooter
+    sFooter,
+    searchBar
   },
   data(){
     return{
@@ -65,49 +51,7 @@ export default {
     
   },
   methods:{
-    goNextPage(){
-      let path='/'+this.routerPath[this.platform]
-      if(this.inputVal.startsWith('https:')){
-        let shortcode=this.splitURLByPlatform()
-        this.$router.push(path+'/post/shortcode/'+shortcode)
-      }else{
-        
-        this.$router.push(path+'/post/user/'+this.inputVal)
-      }
-    },
-    querySearchAsync(queryString,cb){
-      if(queryString.startsWith('http:')||queryString.startsWith('https:')){
-        return
-      }
-     getInsSearchResult(this.inputVal)
-     .then(res=>{
-     console.log("TCL: querySearchAsync -> res", res)
-     let result=[]
-
-      if(res.status==='ok'){
-        for(let i=0;i<10;i++){
-          let k=res.users[i]
-          if(k===undefined){break}
-          let obj={name:k.user.username,src:k.user.profile_pic_url}
-          result.push(obj)
-        }
-        cb(result)
-      }
-     })
-    },
-    handleSelect(item){
-      console.log("TCL: handleSelect -> item", item)
-      this.inputVal=item.name
-    },
-    splitURLByPlatform(){
-      //https://www.instagram.com/p/BymN1ovnItK/
-      switch (this.platform) {
-        case 'Instagram':
-          return this.inputVal.split('https://www.instagram.com/p/')[1].split('/')[0]
-        default:
-          break;
-      }
-    }
+   
   }
 }
 </script>
@@ -147,96 +91,7 @@ export default {
 .home .top h2{
   color:#00FFFA;
 }
-.home .el-autocomplete{
-  width:100%;
-  display: flex;
-  justify-content: center;
-}
-.home .el-input{
-  width: 41.67%;
-  height: 100px;
-  position: relative;
-}
-.home .el-input input{
-  width: 100%;
-  height: 100%;
-  border-radius: 68px;
-  padding-left: 42px;
-  box-sizing: border-box;
-  font-size: 24px;
-  font-family: Arial, Helvetica, sans-serif;
-  color:#AEAEAE;
-}
-.home .el-input input::placeholder{
-  color:#CFCFCF !important;
-  font-size: 24px;
-  opacity: 1;
-  padding: 1px;
-  line-height: 200%;
-}
-.home .el-input  .el-input__suffix{
-  display: block;
-  width:22.75%;
-  height:85%;
-  position: absolute;
-  right: 9px;
-  top:50%;
-}
-.home .el-input  .el-input__suffix-inner{
-  display: block;
-  width: 100%;
-  height: 100%;
-}
-.home .top  .afterQuery{
-  height: 50px;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #efefef;
-  position: static;
-}
-.home .top  .afterQuery img{
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  margin-right: 20px;
-}
-.home .top  .afterQuery p{
-  font-size: 18px;
-  font-family: 'Lalezar', cursive;
-  color:#AEAEAE;
-}
-.home .select{
-  margin-top: 60px;
-  height: 35px;
-}
-.home .select .el-radio__label{
-  color:#fff;
-  font-size:22px;
-  font-family: 'Lalezar', cursive;
-  line-height:35px;
-}
-.home .select .el-radio__inner{
-  width: 16px;
-  height: 16px;
-  border-color:#fff;
-  background-color: transparent;
-}
-.home .select  .el-radio__inner::after{
-  width: 6px;
-  height: 6px;
-}
-.home .search{
-  display: block;
-  width: 100%;
-  height: 100%;
-  transform: translateY(-50%);
-  background:linear-gradient(360deg,#EB6A81 0%,#FFC640 100%);
-  border-radius:68px;
-  font-size:36px;
-  font-family: 'Lalezar', cursive;
-  font-weight:400;
-  color:#fff;
-}
+
 .home .top .leftbg,
 .home .top .rightbg{
   display: block;
