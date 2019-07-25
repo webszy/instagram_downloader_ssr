7d5f93e920789a486413d450a5164207bfa2332f
webszy <sunzhenyu@live.com>
2019-07-25 16:01:18 +0800

    获取媒体详情接口放到node中，但是如果请求次数过于频繁，会被超时，因此暂时不使用

--- server/Routers.js
+++ server/Routers.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const inScraper=require('../utils/insScraper')
 router.get('/', function(req, res, next) {
   res.send(' Priavte Data Provider for Webszy')
+
 });
+// websocket for instagram stroy
 router.ws('/cilent',(ws,req)=>{
     ws.send('connect successfully')
     
@@ -16,5 +19,11 @@ router.ws('/cilent',(ws,req)=>{
      
     })
 })
-
+// 获取某个帖子的详细信息
+router.get('/ins/detail',async (req,res)=>{
+  let shortcode=req.query.shortcode
+  console.log("TCL: shortcode", shortcode)
+  let data= await inScraper.getSingleImageInfo(shortcode)
+  res.json(data)
+})
 module.exports = router;
