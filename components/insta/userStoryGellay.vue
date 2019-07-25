c70640de346f238ce3e442dc4eb9a9b825409781
webszy <sunzhenyu@live.com>
2019-07-25 11:27:12 +0800

    story下载页面

---
+++ components/insta/userStoryGellay.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class='postGellay'>
+    <div  v-for="(k,i) in userMedia" :key=i :class="getItemIndex(i)">
+      <picture-item 
+        :title=k.title 
+        :src=k.display_url 
+        :fileName="getFileName(k)"
+        v-if="k.__typename!=='GraphVideo'" 
+        />
+       <video-player 
+        :title=k.title 
+        :src=k.video_url 
+        :fileName="getFileName(k)"
+        :postImg=k.display_url 
+        v-else
+       />
+    </div>
+   
+  
+  
+  </div>
+</template>
+
+
+<script>
+import pictureItem from './picture'
+import videoPlayer from './videoPlayer'
+export default {
+name:'userPostGellay',
+components:{pictureItem,videoPlayer},
+data(){
+return {
+
+  }
+ },
+props:{
+  userMedia:{
+    type:Array,
+    required:true
+  }
+},
+mounted(){},
+methods:{
+   getMediaSrc(item){
+      if(item.thumbnail_resources&&item.thumbnail_resources[0]){
+        return item.thumbnail_resources[0].src
+      }else{
+         return item.display_url
+      }
+    },
+    getFileName(k){
+      let username=this.$route.params.name || ''
+      return username+'_'+k.shortcode
+    },
+    getItemIndex(i){
+      let str='item '
+      if(i%4===0){
+        str+='itemFirst'
+      }else if(i%4===3){
+        str+='itemLast'
+      }else{
+        str+=''
+      }
+      return str
+    }
+ },
+computed:{
+  
+}
+}
+</script>
+<style>
+.postGellay{
+  width: 100%;
+  /* margin: 0 auto; */
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  padding: 0 13.54%;
+  padding-top: 52px;
+  box-sizing: border-box;
+  
+}
+.postGellay .item{
+  width: 300px;
+  height: 300px;
+  /* margin: auto; */
+  margin-right: 3.49%;
+  margin-bottom: 60px;
+}
+.postGellay .itemFirst{
+  margin-left:0
+}
+.postGellay .itemLast{
+  margin-right:0
+}
+/* .postGellay .picture{
+  margin-right: 60px;
+}
+.postGellay .picture:nth-child(3n){
+  margin-right:0;
+} */
+
+</style>
